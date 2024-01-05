a9fa915c98dcd609ee25750f8897dd85d13507c7
Your Name <timshak@gmail.com>
2024-01-05 06:16:04 +0300

    Update сode

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const chokidar = require('chokidar'); // you need to install it first
+const chokidar = require('chokidar');
 
 let textFiles = {};
 const dirPath = '\\Users\\samsung\\Deep.project\\sync-file\\deep-files-sync\\dirPath\\';
@@ -29,7 +29,13 @@ fs.readdir(dirPath, (err, files) => {
 });
 
 // File watcher
-chokidar.watch(dirPath).on('change', (filePath) => {
+const watcher = chokidar.watch(dirPath);
+
+// track the state of pending changes
+let pendingChanges = {};
+
+// on file added
+watcher.on('add', (filePath) => {
     if(path.extname(filePath).toLowerCase() === '.txt') {
         const file = path.basename(filePath);
         fs.readFile(filePath, 'utf8', (err, data) => {
@@ -37,11 +43,44 @@ chokidar.watch(dirPath).on('change', (filePath) => {
                 console.error(`Error reading file: ${err}`);
                 return;
             }
-            // update file content in textFiles object
+
+            // add new file to textFiles object
             textFiles[file] = data;
-            console.log(`Updated content of ${file}`);
+            console.log(`Added ${file}`);
             console.log(textFiles);
         });
     }
 });
 
+// on file removed
+watcher.on('unlink', (filePath) => {
+    const file = path.basename(filePath);
+    // remove file from textFiles object
+    delete textFiles[file];
+    console.log(`Deleted ${file}`);
+    console.log(textFiles);
+});
+
+watcher.on('change', (filePath) => {
+    if(!pendingChanges[filePath]) {
+        pendingChanges[filePath] = setTimeout(() => {
+            if(path.extname(filePath).toLowerCase() === '.txt') {
+                const file = path.basename(filePath);
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if (err) {
+                        console.error(`Error reading file: ${err}`);
+                        return;
+                    }
+
+                    // update file content in textFiles object
+                    textFiles[file] = data;
+                    console.log(`Updated content of ${file}`);
+                    console.log(textFiles);
+                });
+            }
+
+            // clear the timeout
+            delete pendingChanges[filePath];
+        }, 100);  // 100ms delay to handle rename case
+    }
+});
