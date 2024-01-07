0cade858d23b4bf75a1de11a5bc1e2d4bf4e9b28
Your Name <timshak@gmail.com>
2024-01-07 12:06:29 +0300

    update index 6 try add use deep 1

--- index6.js
+++ index6.js
@@ -5,12 +5,48 @@ const path = require('path');
 let files = {};
 let inodeMap = {};
 
-let dirPath = '\\Users\\samsung\\Deep.project\\sync-file\\deep-files-sync\\dirPath\\'; // Your directory path
+const token = process.argv[2];
+let dirPath = process.argv[3];
+const spaceIdArgument  = process.argv[4];
+
+dirPath = '\\Users\\samsung\\Deep.project\\sync-file\\deep-files-sync\\dirPath\\'; // Your directory path
 
 let pendingRenames = {};
 
-function receptionArgumentsStarts(tokenDeep, spaceId, fileData ){
 
+
+
+const makeDeepClient = (token) => {
+    if (!token) throw new Error('No token provided');
+    const decoded = parseJwt(token);
+    const linkId = decoded?.userId;
+    const apolloClient = generateApolloClient({
+      path: GQL_URN,
+      ssl: !!+GQL_SSL,
+      token,
+    });
+  
+    const deepClient = new DeepClient({ apolloClient, linkId, token });
+    return deepClient;
+  }
+
+async function addedTextLinks(fileData){
+    const syncTextFileTypeId = await deep.id('@deep-foundation/core', 'SyncTextFile');
+    const syncTextFile = (await deep.insert({
+    type_id: syncTextFileTypeId,
+    }, { name: 'INSERT_HANDLER_SYNC_TEXT_FILE' })).data[0];
+    const syncTextFileValue = (await deep.insert({ link_id: syncTextFile?.id, value: fileData }, { table: 'strings' })).data[0];
+    console.log(fileData);
+}
+async function addedContainLinks(spaceIdArgument){
+    const spaceId = spaceIdArgument || (await deep.id('deep', 'admin'));
+    const containTypeId = await deep.id('@deep-foundation/core', 'Contain');
+    const spaceContainSyncTextFile = (await deep.insert({
+    from_id: spaceId,
+    type_id: containTypeId,
+    to_id: syncTextFile?.id,
+    }, { name: 'INSERT_SYNC_TEXT_FILE_CONTAIN' })).data[0];
+    console.log(spaceIdArgument);
 }
 
 // Handle events
@@ -37,6 +73,9 @@ function handleFileChange(absoluteFilePath, current, previous) {
             else {
                 const fileData = fs.readFileSync(absoluteFilePath, { encoding: 'utf8' });
                 files[absoluteFilePath] = fileData;
+
+                addedTextLinks(fileData);
+                addedContainLinks(spaceIdArgument);
 
                 console.log(`File ${currentFileName} added`);
                 console.log(JSON.stringify(files, null, 2));
@@ -75,6 +114,7 @@ watch.watchTree(dirPath, { interval: 1 }, (f, curr, prev) => {
     //console.log(f, curr, prev);
     if (typeof f === "object") {
         // Initial scanning complete
+        makeDeepClient(token);
         const filesStats = f;
         Object.keys(filesStats).forEach(fileName => {
             if (fileName == dirPath) {
@@ -91,4 +131,3 @@ watch.watchTree(dirPath, { interval: 1 }, (f, curr, prev) => {
 });
 
 
-console.log('Hello from my awesome cli!')
